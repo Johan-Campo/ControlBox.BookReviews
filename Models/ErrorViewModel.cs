70307f20edc4ef6b5cdec13b91f2046b7c0b45ed
Johan-Campo <johancampo12@gmail.com>
2026-07-01 15:35:59 -0500

    chore: remove legacy MVC scaffold in favor of backend/frontend split

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ControlBox.BookReviews.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
